fadd89b5ca13d3a2a3c4549e86098058358372c3
AleksashinPri102 <happywin855@gmail.com>
2023-10-25 02:05:27 +0500

    Добавьте файлы проекта.

---
+++ HilbertCurveApp/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace HilbertCurveApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtDepth = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.picCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picCanvas.Location = new System.Drawing.Point(84, 0);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(350, 300);
+            this.picCanvas.TabIndex = 9;
+            this.picCanvas.TabStop = false;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(20, 385);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(40, 25);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "Go";
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtDepth
+            // 
+            this.txtDepth.Location = new System.Drawing.Point(20, 360);
+            this.txtDepth.Name = "txtDepth";
+            this.txtDepth.Size = new System.Drawing.Size(40, 25);
+            this.txtDepth.TabIndex = 6;
+            this.txtDepth.Text = "1";
+            //
+            // Next Step
+            //
+            this.btnNextStep = new System.Windows.Forms.Button();
+            this.btnNextStep.Location = new System.Drawing.Point(10, 420);
+            this.btnNextStep.Name = "btnNextStep";
+            this.btnNextStep.Size = new System.Drawing.Size(60, 25);
+            this.btnNextStep.TabIndex = 8;
+            this.btnNextStep.Text = "Next";
+            this.btnNextStep.Click += new System.EventHandler(this.btnNextStep_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(20, 340);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(36, 13);
+            this.Label1.TabIndex = 5;
+            this.Label1.Text = "Depth";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 273);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtDepth);
+            this.Controls.Add(this.btnNextStep);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "HibertCurveApp";
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox picCanvas;
+        internal System.Windows.Forms.Button btnGo;
+        internal System.Windows.Forms.TextBox txtDepth;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnNextStep;
+    }
+}
---
+++ HilbertCurveApp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HilbertCurveApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(1000, 800);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        private float LastX, LastY;
+        private Bitmap HilbertImage;
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            int depth = int.Parse(txtDepth.Text);
+            if (depth > 8)
+            {
+                if (MessageBox.Show("A large depth may take a long time to draw (and will be mostly black anyway). Do you want to continue?",
+                    "Continue?", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents();
+
+            float total_length, start_x, start_y, start_length;
+
+            // Определяем, насколько большой мы можем сделать кривую.
+            if (picCanvas.ClientSize.Height < picCanvas.ClientSize.Width)
+            {
+                total_length = (float)(0.9 * picCanvas.ClientSize.Height);
+            }
+            else
+            {
+                total_length = (float)(0.9 * picCanvas.ClientSize.Width);
+            }
+
+            start_x = (picCanvas.ClientSize.Width - total_length) / 2;
+            start_y = (picCanvas.ClientSize.Height - total_length) / 2;
+
+            // Вычисляем длину стороны для данного уровня глубины.
+            start_length = (float)(total_length / (Math.Pow(2, depth) - 1));
+
+            HilbertImage = new Bitmap(picCanvas.ClientSize.Width, picCanvas.ClientSize.Height);
+            picCanvas.Image = HilbertImage;
+
+            using (Graphics gr = Graphics.FromImage(HilbertImage))
+            {
+                // Рисуем кривую.
+                gr.Clear(picCanvas.BackColor);
+                LastX = (int)start_x;
+                LastY = (int)start_y;
+                Hilbert(gr, depth, start_length, 0);
+            }
+
+            // Отображаем результат.
+            picCanvas.Refresh();
+            this.Cursor = Cursors.Default;
+        }
+        private void btnNextStep_Click(object sender, EventArgs e)
+        {
+            int currentDepth = int.Parse(txtDepth.Text);
+            currentDepth++;
+            txtDepth.Text = currentDepth.ToString();
+
+            btnGo_Click(sender, e);
+        }
+        // Рисуем кривую Гильберта.
+        private void Hilbert(Graphics gr, int depth, float dx, float dy)
+        {
+            if (depth > 1) Hilbert(gr, depth - 1, dy, dx);
+            DrawRelative(gr, dx, dy);
+            if (depth > 1) Hilbert(gr, depth - 1, dx, dy);
+            DrawRelative(gr, dy, dx);
+            if (depth > 1) Hilbert(gr, depth - 1, dx, dy);
+            DrawRelative(gr, -dx, -dy);
+            if (depth > 1) Hilbert(gr, depth - 1, -dy, -dx);
+        }
+
+        // Рисуем линию (LastX, LastY)-(LastX + dx, LastY + dy) и
+        // обновляем LastX = LastX + dx, LastY = LastY + dy.
+        private void DrawRelative(Graphics gr, float dx, float dy)
+        {
+            gr.DrawLine(Pens.Black, LastX, LastY, LastX + dx, LastY + dy);
+            LastX += dx;
+            LastY += dy;
+        }
+    }
+}
